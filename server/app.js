0898cc429235a459df44d93e2966862915dbb55d
sojharo <sojharo@gmail.com>
2015-11-09 00:48:31 +0500

    marketing sites combined 240 corrected for server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
 var config = require('./config/environment');
 var http = require('http');
