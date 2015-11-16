b01489cce8e3718c930d87b197ae1d4e9de6c775
sojharo <sojharo@gmail.com>
2015-11-16 15:59:48 +0500

    login

--- server/routes.js
+++ server/routes.js
@@ -19,7 +19,8 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      console.log(req.get('host'))
+      console.log(req.get('host'));
+      console.log('sojharo');
       if(req.get('host') == 'www.cloudkibo.com')
         res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
       else if(req.get('host') == 'www.kibosupport.com')
